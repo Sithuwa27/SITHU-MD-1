692460d2dfc16fcb6ff3c6737e7755fa3bc1ebf9
Sithum Kalhara <sithumkalhara271@gmail.com>
2026-05-20 06:45:10 +0000

    The new yt-dlp binary requires Python 3, but the workspace environment i

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,34 +1,48 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
-    pkgs.yt-dlp
+    pkgs.python3
     pkgs.ffmpeg
+    pkgs.yt-dlp
   ];
+
   # Sets environment variables in the workspace
   env = {};
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "christian-kohler.path-intellisense"
+      "ritwickdey.LiveServer"
     ];
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        npm-install = "npm install";
-      };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
+
+    # Enable previews
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and configure a preview with ID "web" on port 3000
+          command = ["npm" "run" "dev" "--" "--port" "$PORT"];
           manager = "web";
         };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when a workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build files
+        # watch-backend = "npm run watch-backend";
       };
     };
   };
