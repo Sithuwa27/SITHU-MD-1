679e88e7bb71049a5fd65cda508607f446c3c3e3
Sithum Kalhara <sithumkalhara271@gmail.com>
2026-05-20 06:49:43 +0000

    The terminal is still missing python3 for the child_process. Please comp

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,48 +1,22 @@
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
     pkgs.python3
     pkgs.ffmpeg
+    pkgs.curl
     pkgs.yt-dlp
   ];
-
-  # Sets environment variables in the workspace
-  env = {};
-
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "ritwickdey.LiveServer"
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-          # and configure a preview with ID "web" on port 3000
-          command = ["npm" "run" "dev" "--" "--port" "$PORT"];
-          manager = "web";
-        };
-      };
-    };
-
-    # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        npm-install = "npm install";
       };
-      # Runs when a workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build files
-        # watch-backend = "npm run watch-backend";
+        # Optional: Run any background tasks on start
       };
     };
   };
